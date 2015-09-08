6ac2f40b06b3ba8c0e7a61e433c77e6175f74545
yize <hanchao.shc@alibaba-inc.com>
2015-09-08 15:35:01 +0800

    update dependencies

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,5 @@
 const Context = require('tingle-context');
 const Demo = require('./demo');
+require('fastclick').attach(document.body);
 
 React.render(<Demo/>, document.getElementById('TingleDemo'));
